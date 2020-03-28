218b2142b1ed0122def60496f6d308192fe734c0
Jillian <Jillian@Jillians-MacBook-Pro-2.local>
2020-03-28 16:34:57 -0400

    adding course-table

---
+++ src/app/course-table/course-table.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {CourseServiceClient} from '../services/CourseServiceClient';
+
+@Component({
+  selector: 'app-course-list',
+  templateUrl: './course-table.component.html',
+  styleUrls: ['./course-table.component.css']
+})
+// <app-course-list/> is how we refer to this
+export class CourseTableComponent implements OnInit {
+
+  title = 'Course List Component'
+
+  courses = []
+
+  newCourseTitle = ''
+
+  constructor(private service: CourseServiceClient ) { }
+
+  createCourse = () => {
+    this.courses.push({_id: Math.random(), title: this.newCourseTitle});
+  }
+
+  deleteCourse = (courseToDel) => {
+    this.courses = this.courses.filter(course => course !== courseToDel);
+  }
+  ngOnInit(): void {
+    this.service.findAllCourses().then(courses => this.courses = courses);
+  }
+
+}
