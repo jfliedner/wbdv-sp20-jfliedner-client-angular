a6d19f245de84631deabc0a9bdc7cd694013051c
Jillian <Jillian@Jillians-MacBook-Pro-2.local>
2020-03-29 16:40:40 -0400

    issue with service

--- src/app/services/CourseServiceClient.ts
+++ src/app/services/CourseServiceClient.ts
@@ -4,18 +4,18 @@ import {Injectable} from '@angular/core';
 export class CourseServiceClient {
 
   findAllCourses = () =>
-    fetch('http://wbdv-generic-server.herokuapp.com/api/001234485/courses')
+    fetch('https://wbdv-generic-server.herokuapp.com/api/001234485/courses')
       .then(response => response.json())
 
   findCourseById = (cid) =>
-    fetch(`http://wbdv-generic-server.herokuapp.com/api/001234485/courses/${cid}`)
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/001234485/courses/${cid}`)
       .then(response => response.json())
 
   findModulesForCourse = (cid) =>
-    fetch(`http://wbdv-generic-server.herokuapp.com/api/001234485/courses/${cid}/modules`)
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/001234485/courses/${cid}/modules`)
       .then(response => response.json())
 
   findLessonsForModule = (mid) =>
-    fetch(`http://wbdv-generic-server.herokuapp.com/api/001234485/modules/${mid}/lessons`)
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/001234485/modules/${mid}/lessons`)
       .then(response => response.json())
 }
